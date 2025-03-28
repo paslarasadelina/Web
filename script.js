3dd46110eaaebd36d56c214fa451cf06ca4add4c
radupalai <steamfurat60@gmail.com>
2025-03-28 09:03:57 +0200

    Scroll

--- script.js
+++ script.js
@@ -100,3 +100,20 @@ adaugaCosButtons.forEach(button => {
 inchideCosButton.addEventListener('click', () => {
     cosElement.style.display = 'none';
 });
+
+////////////////////
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scrollToTop");
+
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > 300) {
+          scrollToTopButton.classList.add("show");
+      } else {
+          scrollToTopButton.classList.remove("show");
+      }
+  });
+
+  scrollToTopButton.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
